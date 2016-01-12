633d6f8b55924a765df7070a8a071bf8786b23cc
smalec <kaczy2406@gmail.com>
2016-01-12 13:14:12 +0100

    Added affine layer and modified layer abstract class

--- modules/layers/AffineLayer.py
+++ modules/layers/AffineLayer.py
@@ -3,28 +3,25 @@ import numpy as np
 from theano import shared
 from Layer import Layer
 from ..initializators.Normal import Normal
-from ..initializators.Constant import Constant
 
 
-class SoftMaxLayer(Layer):
-    def __init__(self, in_size, out_size):
-        super(SoftMaxLayer, self).__init__(in_size, out_size)
+class AffineLayer(Layer):
+    def __init__(self, in_size, out_size, activation=T.nnet.sigmoid):
+        super(AffineLayer, self).__init__(in_size, out_size)
+        self.activation = activation
         self.weights = self.init_weights()
         self.biases = self.init_biases()
 
     def propagate(self, inputs):
         self.inputs = inputs
-        self.outputs = T.nnet.softmax(T.dot(self.inputs, self.weights) + self.biases.dimshuffle('x', 0))
+        self.outputs = self.activation(T.dot(self.inputs, self.weights) + self.biases.dimshuffle('x', 0))
 
     def init_weights(self):
         weights = shared(np.zeros((self.in_size, self.out_size), dtype=np.float32), name='W')
-        weights.tag.initializer = Normal(0.05)
+        weights.tag.initializer = Normal(np.sqrt(1.0/self.out_size))
         return weights
 
     def init_biases(self):
         biases = shared(np.zeros((self.out_size,), dtype=np.float32), name='b')
-        biases.tag.initializer = Constant(0.0)
+        biases.tag.initializer = Normal(1.0)
         return biases
-
-    def cost(self, Y):
-        return - T.log(self.outputs[T.arange(Y.shape[0]), Y]).mean()
--- modules/layers/Layer.py
+++ modules/layers/Layer.py
@@ -1,8 +1,14 @@
 class Layer(object):
+    def __init__(self, in_size, out_size):
+        self.in_size = in_size
+        self.out_size = out_size
+        self.weights, self.biases = None, None
+        self.inputs, self.outputs = None, None
+
     @property
     def parameters(self):
-        return []
+        return [self.weights, self.biases]
 
     @property
     def parameter_names(self):
-        return []
+        return ['W', 'b']
